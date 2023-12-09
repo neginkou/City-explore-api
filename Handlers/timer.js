13b48d9148e2fe3d4e1d421d4e414cc40762dfb2
neginkou <negin.kou@gmail.com>
2023-12-08 23:51:31 -0800

    cleaning codes

---
+++ Handlers/timer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function timer(cache){
+  setTimeout()
+  console.log(`${cache} is empty`);
+  
+};
+
+module.exports = timer;
--- server.js
+++ server.js
@@ -1,34 +1,29 @@
 'use strict';
 
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const weatherData = require('./Data/weather.json');
-const getMovie = require('./Handlers/movie');
-const handleWeather = require('./Data/Handlers/weather');
-const handlemovies = require('./Handlers/movies');
-
+const getWeather = require('./Handlers/weather');
+const handlemovies = require('./Handlers/movies'); 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 
-function HandleHomePage (request, response){
-  response.status(200).send('Access Granted')
+function HandleHomePage(request, response) {
+  response.status(200).send('Access Granted');
 }
-function HandleNotfound(request, response){
-  response.status(404).send('Denied')
+function HandleNotfound(request, response) {
+  response.status(404).send('Denied');
 }
 
 app.get('/', HandleHomePage);
 
-app.get("/weather", handleWeather);
+app.get("/weather", getWeather);
 
-app.get("/movies", handlemovies);
-
+app.get("/movies", handlemovies); 
 
 app.get('*', HandleNotfound);
 
